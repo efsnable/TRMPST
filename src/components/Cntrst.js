ca06762c532f28af5f78801d914ceb93b0e35773
0xefsnable <EFS@lOCALHOST>
2020-11-08 18:53:35 +0530

    NONONONONO NO NO NO NO

--- src/components/Cntrst.js
+++ src/components/Cntrst.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import LevelGenerator from "./LevelGenerator";
 
 const style = {
     height: "80vh" // we can control scene size by setting container dimensions
 };
+
+const wellDepth = 60.0;
+const matColors = [0xff0000, 0x00ff00, 0x0000ff, 0x6f0050, 0x506f00, 0x00506f, 0xff0000, 0x00ff00, 0x0000ff, 0x6f0050, 0x506f00, 0x00506f, 0xff0000, 0x00ff00, 0x0000ff, 0x6f0050, 0x506f00, 0x00506f];
+
 export class Cntrst extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = { x: 0, y: 0, currentLane: 0 };
+      }
+    
+      _onMouseMove(e) {
+        let newLane = Math.floor(this.lanes.length * e.clientX/window.innerWidth);
+
+        this.setState({ x: e.clientX/window.innerWidth, y: e.clientY / window.innerHeight, currentLane: newLane});
+        // console.log("window",  [window.innerWidth,  window.innerHeight ]);
+        // console.log("mouse", [e.clientX, e.clientY ]);
+        // console.log("mouse%", [this.state.x, this.state.y ]);
+      }
+    
     componentDidMount() {
         this.sceneSetup();
         this.addCustomSceneObjects();
@@ -28,7 +48,7 @@ export class Cntrst extends Component {
             75, // fov = field of view
             width / height, // aspect ratio
             0.1, // near plane
-            100 // far plane
+            wellDepth* 3 // far plane
         );
         this.camera.position.z = 15; // is used here to set some distance from a cube that is located at z = 0
 
@@ -45,7 +65,6 @@ export class Cntrst extends Component {
     // Code below is taken from Three.js BoxGeometry example
     // https://threejs.org/docs/#api/en/geometries/BoxGeometry
     addCustomSceneObjects = () => {
-        const matColors = [0xff0000, 0x00ff00, 0x0000ff, 0x6f0050, 0x506f00, 0x00506f];
 
         const material = new THREE.MeshPhongMaterial({
             color: 0x156289,
@@ -60,14 +79,22 @@ export class Cntrst extends Component {
         var multiMaterial = [darkMaterial, wireframeMaterial];
 
 
-        const laneCoords = [[2, 0],
-        [5, 4], // 129 deg, 5' len
-        [9, 6],  // 153 deg, 4.5' len
-        [2, 10]  // 56 deg, 8.625' len
+        const laneCoords = LevelGenerator.generateMirrored();
+        console.log("Generated: ", laneCoords);
+        
+        // [[2, 0],
+        // [5, 4], // 129 deg, 5' len
+        // [9, 6],  // 153 deg, 4.5' len
+        // [2, 10],  // 56 deg, 8.625' len
+        // [0, 8],
+        // [-2, 10],
+        // [-9, 6],
+        // [-5, 4],
+        // [-2, 0]
+        // ];
 
-        ];
-        var laneX = 0;
-        var laneY = 0;
+        var laneX = laneCoords[laneCoords.length-1][0];
+        var laneY = laneCoords[laneCoords.length-1][1];
         const endX = 10;
         const endY = 10;
 
@@ -75,11 +102,14 @@ export class Cntrst extends Component {
         this.pos = [];
         this.rot = [];
         this.lanes = [];
+        this.laneMaterials = [];
+        
 
         var colorctr = 0;
 
         const getAngle = (anchor, point) => Math.atan2(anchor.y - point.y, anchor.x - point.x);
 
+        this.lanesGroup =  new THREE.Group();
 
         laneCoords.forEach(coords => {
             let mx = coords[0];
@@ -90,25 +120,29 @@ export class Cntrst extends Component {
 
             let len = Math.sqrt(dx * dx + dy * dy);
             console.log(len, " len from ", coords)
-            var geometry = new THREE.BoxBufferGeometry(0.0, len, 12);
+            var geometry = new THREE.BoxBufferGeometry(0.0, len, wellDepth);
             geometry.computeBoundingBox();
-            console.log("size", geometry.boundingBox.getSize());
+            //console.log("size", geometry.boundingBox.getSize());
            
-            geometry.translate(0-geometry.boundingBox.getSize().x/2,0- geometry.boundingBox.getSize().y/2,0 );
+            geometry.translate(0-geometry.boundingBox.getSize().x/2,
+                                0- geometry.boundingBox.getSize().y/2,
+                                0- geometry.boundingBox.getSize().z /2 );
 
             this.geoms.push(geometry);
             this.pos.push(coords);
             let angle = getAngle({ x: laneX, y: laneY }, { x: mx, y: my });
 
             this.rot.push(angle);
-            console.log(colorctr)
+            //console.log(colorctr)
             let mat = new THREE.MeshPhongMaterial({
                 color: matColors[colorctr],
                 emissive: matColors[colorctr++],
                 side: THREE.DoubleSide,
-                flatShading: true
+                flatShading: true,
+                transparent: true
             });
 
+            
             let lane = new THREE.Mesh(geometry, mat);
 
             //lane.rotation.x = 1.5;
@@ -124,25 +158,33 @@ export class Cntrst extends Component {
 
             let box = new THREE.BoxHelper(lane, 0xffff00);
             lane.geometry.computeBoundingBox();
-            console.log(colorctr, lane.geometry.boundingBox);
+            //console.log(colorctr, lane.geometry.boundingBox);
 
 
                 laneX = mx;
                 laneY = my;
-            console.log("coords", laneX, laneY);
+            //console.log("coords", laneX, laneY);
 
             this.lanes.push(lane);
+            this.laneMaterials.push(mat);
+            this.lanesGroup.add(lane);
             ///this.scene.add(box);
-            this.scene.add(lane);
 
 
         });
+
+
+        this.scene.add(this.lanesGroup);
+        let groupBox = new THREE.Box3().setFromObject(this.lanesGroup);
+       // console.log("group size", groupBox.getSize());
+        this.camera.position.y =  groupBox.getSize().y/2;
+        this.controls.target.set(0, groupBox.getSize().y/2 + 2, -3)
+        this.controls.update();
         const lmaterial = new THREE.LineBasicMaterial( { color: 0xf0f0ff } );
-        const lineZ = 6.1;
+        const lineZ = 0.1;
         const points = [new THREE.Vector3(0,  0, lineZ )];
 
         laneCoords.forEach(coords => {
-
             points.push( new THREE.Vector3(coords[0], coords[1], lineZ ) );
         });
 
@@ -168,9 +210,20 @@ export class Cntrst extends Component {
         // this.cube.rotation.x += 0.01;
         // this.cube.rotation.y += 0.01;
 
-        this.lanes.forEach(lane => {
-            //lane.rotation.x += 0.01
-        });
+
+        for (let i = 0; i < this.lanes.length; i++) {
+            const lane = this.lanes[i];
+            if(lane!= this.lanes[this.state.currentLane]){
+                    lane.material.opacity = 0.1;
+            }else{
+                lane.material.opacity = 1.0;
+            }
+        }
+
+
+//        this.lanes[this.state.currentLane].opacity= 1.0;
+
+       // console.log("target", this.controls.target);
 
         this.renderer.render(this.scene, this.camera);
 
@@ -195,7 +248,7 @@ export class Cntrst extends Component {
     };
     render() {
         return (
-            <div style={style} ref={ref => (this.el = ref)} />
+            <div style={style} ref={ref => (this.el = ref)}  onMouseMove={this._onMouseMove.bind(this)} />
         )
     }
 }
